03e36b0e78abed999383b58b10ee4d400858781a
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-06-20 21:28:33 +0900

    합 재귀함수
    합 재귀함수를 작성한 알고리즘

---
+++ Recursive Sum.R
@@ -0,0 +1,50 @@
+### Algorithm : 합 재귀함수 ###
+
+## 방법 1 : 1 + 2 + ... + n
+# (1) User-defined function
+asum <- function(n) {
+  s <- 0
+  for(i in 1:n) { # 입력형태 : integer n
+    s <- s + i
+  }
+  return(s)
+}
+
+# (2) Result Check
+asum(55)
+
+## 2. Code
+sum.recursive <- function(n) {
+  if(n == 1) return(n)
+  return(n + sum.recursive(n-1)) #n + summation(n-1)
+}
+
+## 3. Result Check
+sum.recursive(55)
+
+##########################################################################
+
+## 방법 2 : x[1] + x[2] + ... x[n]
+# (1) User-defined function
+bsum <- function(x) {
+  s <- 0
+  for(i in 1:length(x)) {
+    s <- s + x[i]
+  }
+  return(s)
+}
+
+# (2) Result Check
+bsum(55)
+bsum(1:100)
+bsum(c(4, 6, 7, 8))
+
+## 2. Code
+sum.recursive2 <- function(x) {
+  if(length(x) == 1) return(x)
+  return(x[1] + sum.recursive2(x[-1]))
+}
+
+## 3. Result Check
+sum.recursive2(1:100)
+sum.recursive2(c(4, 6, 8, 9))
