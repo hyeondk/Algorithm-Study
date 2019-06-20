6ec5f0fd14cc0ec749ebc8095ee203ec18fca879
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-06-20 22:05:06 +0900

    팩토리얼 재귀함수
    *변경점*
    1. line 8, 19, 20에 있는 comment에 있는 #과 글자간 공백이 없어 공백을 추가하였음.

--- 2. 재귀 알고리즘/Recursive Factorial.R
+++ 2. 재귀 알고리즘/Recursive Factorial.R
@@ -5,7 +5,7 @@
 fact <- function(n) {
   s <- 1
   for(i in 1:n) {
-    s <- s * i #합에 i를 계속 곱한다.
+    s <- s * i # 합에 i를 계속 곱한다.
   }
   return(s)
 }
@@ -16,8 +16,8 @@ fact(10)
 
 ## 2. Code
 fact.recursive <- function(n) {
-  if(n == 1) return(1) #n = 1인 경우를 꼭 세팅해주어야 함.
-  return(n * fact.recursive(n-1)) #n x (n-1)!
+  if(n == 1) return(1) # n = 1인 경우를 꼭 세팅해주어야 함.
+  return(n * fact.recursive(n-1)) # n x (n-1)!
 }
 
 ## 3. Result Check
