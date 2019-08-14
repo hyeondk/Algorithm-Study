ee546eb77bbe841faa701ca13f6d3c13f4f12710
Donghyeon Kim <49542452+Pacifist2553@users.noreply.github.com>
2019-08-14 18:03:38 +0900

    2진법 전환
    11과 9를 2진법으로 전환했을 때 값이 똑같이 나오는 점을 해결하였음.
    기존에 작성한 while(k == 2) 문을 while(k >= 2)로 수정하였음.
    line 14 참고바람.

--- 1. n진법 전환 알고리즘/Binary Method.R
+++ 1. n진법 전환 알고리즘/Binary Method.R
@@ -11,11 +11,11 @@ find2num <- function(n) {
   x <- vector(mode = "numeric", length = k) # 빈 벡터 생성
   x[k] <- n %% 2 # k번째에 나머지
   q <- n %/% 2 # q라는 변수에 몫 입력
-  while(k == 2) {
+  while(k >= 2) {
     k <- k - 1 # 자릿수 하나씩 낮춰서 x[4]부터 값 입력
     x[k] <- q %% 2
     q <- q %/% 2
-  } # k == 2까지 반복
+  } # k = 2까지 반복
   x[1] <- 1 # 이진수의 1번째 값은 1
   
   return(as.integer(x)) # 이진수 출력
@@ -24,4 +24,5 @@ find2num <- function(n) {
 ## 2. Result Check
 find2num(17)
 find2num(11)
+find2num(9)
 find2num(233)
